16533843179be5f443bd992c959773409653c30c
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-05 15:02:05 +0530

    updated

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,31 +1,9 @@
-// import Postcard from "@/app/components/postcard/postcard"
-// import { getPosts } from "../lib/data"
+import React from 'react'
 
-// //FETCH WITH AN API
-// const getdata=async ()=>{
-//   const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
-   
-//   if(!res.ok)
-//   throw new Error("Something went wrong")
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
 
-//  return res.json()
-// }
-// const blogpage = async () => {
-//   //FETCH WITH AN API
-//  // const posts= await getdata()
-
-//  //FETCH WITHOUT AN API
-//   const posts= await getPosts()
-//   return (
-//     <div className="flex flex-wrap gap-7">
-//       {posts.map((post)=>(<div className="md:w-[40%] lg:max-w-[1280px] md:max-w-[768px] lg:w-[30%] w-full" key={post.id}>
-//         <Postcard post={post}/>
-//       </div>))}
-      
-      
-    
-//     </div>
-//   )
-// }
-
-// export default blogpage
+export default page
